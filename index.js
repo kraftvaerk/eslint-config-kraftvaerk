a3df641c1ec3729b583ce184f9b81795f7355198
Mithun Dhiman <mi2oon@gmail.com>
2016-03-03 15:19:10 +0100

    Duplicate data property in object literal not allowed in strict mode

--- index.js
+++ index.js
@@ -117,8 +117,6 @@ module.exports = {
         'no-invalid-this': 0,
         // disallow usage of __iterator__ property
         'no-iterator': 2,
-        // disallow use of labeled statements
-        'no-labels': 2,
         // disallow unnecessary nested blocks
         'no-lone-blocks': 2,
         // disallow creation of functions within loops
